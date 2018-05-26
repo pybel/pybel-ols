bbff0f8f56ce5a81d6c0c6a83c978a770a269930
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-26 18:09:31 +0200

    Update docstring
    Removed accidental redundant references

--- src/pybel_ols/__init__.py
+++ src/pybel_ols/__init__.py
@@ -2,11 +2,13 @@
 
 """A PyBEL [1]_ extension for building BEL resources with the EBI [2]_ Ontology Lookup Service.
 
-Citation
---------
-If you use PyBEL in your work, please cite [1]_:
+References
+----------
+.. [1] Hoyt, C. T., *et al.* (2017). `PyBEL: a Computational Framework for Biological Expression Language
+       <https://doi.org/10.1093/bioinformatics/btx660>`_. Bioinformatics, 34(December), 1–2.
 
-.. [1] Hoyt, C. T., *et al.* (2017). `PyBEL: a Computational Framework for Biological Expression Language <https://doi.org/10.1093/bioinformatics/btx660>`_. Bioinformatics, 34(December), 1–2.
+.. [2] Cote, R., *et al.* (2006). `The Ontology Lookup Service, a lightweight cross-platform tool for controlled
+       vocabulary queries <https://doi.org/10.1186/1471-2105-7-97>`_. BMC Bioinformatics, 7, 1–7.
 
 Installation
 ------------
@@ -27,15 +29,6 @@ Clone the repository from `GitHub <https://github.com/pybel/pybel-ols>`_ and ins
    $ git clone https://github.com/pybel/pybel-ols.git
    $ cd bio2bel
    $ python3 -m pip install -e .
-
-References
-----------
-.. [1] Hoyt, C. T., *et al.* (2017). `PyBEL: a Computational Framework for Biological Expression Language
-       <https://doi.org/10.1093/bioinformatics/btx660>`_. Bioinformatics, 34(December), 1–2.
-
-.. [2] Cote, R., *et al.* (2006). `The Ontology Lookup Service, a lightweight cross-platform tool for controlled
-       vocabulary queries <https://doi.org/10.1186/1471-2105-7-97>`_. BMC Bioinformatics, 7, 1–7.
-
 """
 
 from .ols_utils import (
